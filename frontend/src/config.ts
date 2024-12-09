9671a76d4dc6e391c96683926ddd662f28234ecd
Jose Arrillaga <jarrilla@proton.me>
2024-12-09 09:15:48 -0500

    add env var for baseURL

---
+++ frontend/src/config.ts
@@ -0,0 +1,7 @@
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+if (!apiBaseUrl) {
+  throw new Error('VITE_API_BASE_URL environment variable is not set');
+}
+
+export const API_BASE_URL = apiBaseUrl; 

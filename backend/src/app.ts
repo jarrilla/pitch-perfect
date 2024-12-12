13c65c6ccdf954a8edff0fa34f24d89f78ab771c
Jose Arrillaga <jarrilla@proton.me>
2024-12-12 18:30:23 -0500

    add name back

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,6 +29,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET!,
   resave: false,
   saveUninitialized: false,
+  name: 'sessionId',
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
